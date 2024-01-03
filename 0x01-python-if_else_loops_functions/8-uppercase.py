0c9610a0af92f80af9529623e6e9f6186dc1ea80
Bemin <bemin_dawoud@yahoo.com>
2024-01-02 18:19:44 -0800

    update

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for pos in range(0, len(str)):
+        code = ord(str[pos])
+        if code > 96 and code < 123:
+            code = code - 32
+        print("{:s}".format(chr(code)), end='')
+    print("{:s}".format(''))
