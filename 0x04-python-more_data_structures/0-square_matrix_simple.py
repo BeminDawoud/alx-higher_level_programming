0b9a8133a921dcd6430a72d486365545eaf572a4
Bemin <bemin_dawoud@yahoo.com>
2024-01-10 18:08:46 -0800

    update

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,6 @@
+def square_matrix_simple(matrix=[]):
+    new = list(map(list, matrix))
+    for i in range(len(new)):
+        for j in range(len(new[i])):
+            new[i][j] = new[i][j] ** 2
+    return new
