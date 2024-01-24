6a24a68f008f19a33ee99f6792d016e284d3a647
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-01-24 03:45:28 +0200

    adding task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    this is a Square class
+    """
+    def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def size(self):
+        return self.__size
+
+    def size(self, value):
+        self.__size = value
+    def area(self):
+        return self.__size**2
