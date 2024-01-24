95798e708708e4c49301458b3d90e57a6e1c158d
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-01-24 03:37:18 +0200

    updating task 2 to raise exceptions

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -9,4 +9,8 @@ class Square:
     this is a Square class
     """
     def __init__(self, size=0):
+        if isinstance(size, int) is False:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         self.__size = size
