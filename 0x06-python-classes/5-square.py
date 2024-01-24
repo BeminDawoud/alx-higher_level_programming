30552bbc9723bd241126f3e67fe28c0a262c1dfd
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-01-24 04:07:30 +0200

    adding task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    this is a Square class
+    """
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter"""
+        if isinstance(value, int) is False:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """calculate area"""
+        return self.__size**2
+
+    def my_print(self):
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end='')
+            print('')
