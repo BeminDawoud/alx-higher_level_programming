3366970042fc85f9e95e2856d3f1c17df7d483ec
Bemin Dawoud <bemin_dawoud@yahoo.com>
2024-01-24 03:34:58 +0200

    adding task 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a Square class.
+"""
+
+
+class Square:
+    """
+    this is a Square class
+    """
+    def __init__(self, size=0):
+        self.__size = size
