0a8030b145f09c859ecaee6c0b8f1c0afce71a95
Bemin <bemin_dawoud@yahoo.com>
2024-04-08 18:52:43 -0700

    5-integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /* comment */
-const args = process.argv.length;
-if (parseInt(args[3])) {
-  console.log('My number:', parseInt(args[3]));
+const args = process.argv;
+if (parseInt(args[2])) {
+  console.log('My number:', parseInt(args[2]));
 }
 else {
   console.log('Not a number');
