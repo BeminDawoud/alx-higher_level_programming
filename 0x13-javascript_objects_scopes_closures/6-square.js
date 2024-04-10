46b0a57d9f453d0ca61e245d7a73dca776098134
Bemin <bemin_dawoud@yahoo.com>
2024-04-09 19:47:39 -0700

    Create 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+/* comment */
+const Rectangle = require('./4-rectangle');
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c = 'X') {
+    if (this.width && this.height) {
+      for (let row = 0; row < this.height; row++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
