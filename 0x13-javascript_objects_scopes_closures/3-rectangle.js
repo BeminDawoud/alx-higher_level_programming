806f081942806267e37d74faa2400c83d4131de2
Bemin <bemin_dawoud@yahoo.com>
2024-04-09 19:20:28 -0700

    Create 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/* comment */
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    if (this.width && this.height) {
+      for (let row = 0; row < this.height; row++) {
+        console.log('X'.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Rectangle;
