31ef744b9ad1bb9c8fcbaa2a622ea7316a51eed1
Bemin <bemin_dawoud@yahoo.com>
2024-04-14 07:29:16 -0700

    create 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(host = 'localhost', port = 3306, user = user, passwd = pwd, db = database)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
+
