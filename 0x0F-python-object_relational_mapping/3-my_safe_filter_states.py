99758266c737c3e812f75a1ce84e4ebc97d205fb
Bemin <bemin_dawoud@yahoo.com>
2024-04-14 10:11:31 -0700

    create 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+0-select_states.py
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    database = sys.argv[3]
+    arg = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=user,
+            passwd=pwd,
+            db=database
+            )
+    cur = db.cursor()
+    cur.execute(
+            "SELECT * FROM states WHERE BINARY name = %s"
+            "ORDER BY states.id ASC", (arg,)
+            )
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
