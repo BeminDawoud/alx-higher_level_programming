03734a81bd8fac7f7b5b2d2dca46ae0d07755b8f
Bemin <bemin_dawoud@yahoo.com>
2024-04-14 07:51:26 -0700

    create 1

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+0-select_states.py
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+            host='localhost',
+            port=3306,
+            user=user,
+            passwd=pwd,
+            db=database
+            )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    states = cur.fetchall()
+    for state in states:
+        print(state)
+    cur.close()
+    db.close()
