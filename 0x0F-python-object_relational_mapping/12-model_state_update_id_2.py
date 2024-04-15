ac8cc7b41670e23bcf55ef1b8c7b1111966f72c0
Bemin <bemin_dawoud@yahoo.com>
2024-04-14 20:09:24 -0700

    Create 12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Comment
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+            f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{database}',
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    res = session.query(State).filter(State.id == 2).first()
+    res.name = 'New Mexico'
+    session.commit()
