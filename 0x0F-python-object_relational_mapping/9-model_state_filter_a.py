f278c6198716708f582634512905e45b79cceb21
Bemin <bemin_dawoud@yahoo.com>
2024-04-14 19:47:29 -0700

    Create 9

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Comment
+"""
+import sys
+from model_state import Base, State
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    pwd = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+            f'mysql+mysqldb://{user}:{pwd}@localhost:3306/{database}',
+            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for res in session.query(State).filter(State.name.like('%a%')):
+        print(f"{res.id}: {res.name}")
