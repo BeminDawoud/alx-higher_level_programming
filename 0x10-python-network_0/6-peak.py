42f405c59412a489c71c26c2aef41d17594b657a
Bemin <bemin_dawoud@yahoo.com>
2024-04-27 10:31:47 -0700

    Update comments

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,6 +5,10 @@ finds a peak in a list of unsorted integers.
 
 
 def find_peak(list_of_integers):
+    '''
+    function that finds a peak of a list
+    '''
+
     if list_of_integers and isinstance(list_of_integers, list):
         return max(list_of_integers)
     return None
