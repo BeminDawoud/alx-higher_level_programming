bb98a02a470bfa655eedb83646f52afa40f1bc04
Bemin <bemin_dawoud@yahoo.com>
2024-04-28 18:47:38 -0700

    Create

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,15 +2,12 @@
 '''
 script that fetches https://alx-intranet.hbtn.io/status
 '''
+
+
 import urllib.request
 
 
 if __name__ == "__main__":
-    '''
-    Main Script
-    '''
-
-
     url = 'https://alx-intranet.hbtn.io/status'
     resp = urllib.request.urlopen(url)
     html = resp.read()
