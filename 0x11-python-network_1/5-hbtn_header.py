53c293909ad08975f6ebcb4b2315ef711b3a0c7d
Bemin <bemin_dawoud@yahoo.com>
2024-04-28 19:43:30 -0700

    Create 5wq

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in a URL
+sends a request to the URL
+displays the value of the X-Request-Id variable
+"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = requests.get(url)
+    print(req.headers['X-Request-Id'])
