b695687caa556a7ea4caa66d757a0ae026505d8d
Bemin <bemin_dawoud@yahoo.com>
2024-04-28 19:28:26 -0700

    Create 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+takes in a URL
+sends a request to the URL
+"""
+
+
+from urllib import request, parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    data = {'email' : sys.argv[2]}
+    data = parse.urlencode(data)
+    data = data.encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as resp:
+        print(resp.read().decode('utf-8'))
