3f3335a3198a978036151d4faaad60b7b8e4cfa6
Bemin <bemin_dawoud@yahoo.com>
2024-04-28 18:44:43 -0700

    Create

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     resp = urllib.request.urlopen(url)
     html = resp.read()
+    resp.close()
     print('Body response:')
     print(f"\t- type: {type(html)}")
     print(f"\t- content: {html}")
     print(f"\t- utf8 content: {html.decode('utf8')}")
-    resp.close()
