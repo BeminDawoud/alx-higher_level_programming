7c6fceec3fce60ad1d83d935517256678e38dcd0
Bemin <bemin_dawoud@yahoo.com>
2024-05-21 17:37:54 -0700

    create 4-starcount

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+/* display the status code of a GET request. */
+const request = require('request');
+let count = 0;
+const actor = 'https://swapi-api.alx-tools.com/api/people/18/';
+request('https://swapi-api.alx-tools.com/api/films/', (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const films = JSON.parse(body).results;
+    for (const film of films) {
+      if (film.characters.includes(actor)) {
+        count++;
+      }
+    }
+    console.log(count);
+  }
+});
