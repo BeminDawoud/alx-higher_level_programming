093ed0cc03a043f3f7e2583bd1b63d38ac218488
Bemin <bemin_dawoud@yahoo.com>
2024-05-21 18:59:25 -0700

    create advanced

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,19 +2,32 @@
 /*  prints all characters of a Star Wars movie:. */
 const request = require('request');
 const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+
+function makeRequest(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(JSON.parse(body).name);
+      }
+    });
+  });
+}
+
 request(url, (error, response, body) => {
   if (error) {
     console.error(error);
   } else {
     const chars = JSON.parse(body).characters;
-    for (const char of chars) {
-      request(char, (error, response, body) => {
-        if (error) {
-          console.error(error);
-        } else {
-          console.log(JSON.parse(body).name);
-        }
+    const promises = chars.map(char => makeRequest(char));
+
+    Promise.all(promises)
+      .then(names => {
+        names.forEach(name => console.log(name));
+      })
+      .catch(error => {
+        console.error(error);
       });
-    }
   }
 });
